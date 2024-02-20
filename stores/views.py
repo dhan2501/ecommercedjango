abb71cbe098dce5a2a4166507a81e77a35178537
Dhananjay Gupta <82753264+dhan2501@users.noreply.github.com>
2024-02-20 20:02:45 +0530

    show product home pae

--- stores/views.py
+++ stores/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models.products import Product
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    prds = Product.get_all_products()
+    # print(products)
+    return render(request, 'index.html', {'products' : prds})
