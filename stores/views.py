2d4089238414c386fc799859d42da367553391c2
Dhananjay Gupta <82753264+dhan2501@users.noreply.github.com>
2024-03-13 13:23:23 +0530

    filtter Products by category

--- stores/views.py
+++ stores/views.py
@@ -2,9 +2,21 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from .models.products import Product
+from .models.category import Category
 # Create your views here.
 
 def index(request):
-    prds = Product.get_all_products()
+    # products = Product.get_all_products()
+    products =None
+    categories = Category.get_all_categories()
+    # print(request.GET['category'])
+    categoryID = request.GET.get('category')
+    if categoryID:
+        products = Product.get_all_products_by_categoryid(categoryID)
+    else:
+        products = Product.get_all_products()
+    data = {}
+    data['products'] = products
+    data['categories'] = categories
     # print(products)
-    return render(request, 'index.html', {'products' : prds})
+    return render(request, 'index.html', data)
