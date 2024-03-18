eccf2975f2c6d421c65d78664df9c9a97826e5c2
Dhananjay Gupta <82753264+dhan2501@users.noreply.github.com>
2024-03-18 12:09:54 +0530

    signup form input filed data submitted database successfully

--- stores/admin.py
+++ stores/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 from .models.products import Product
 from .models.category import Category
+from .models.customer import Customer
 
 # Display actual table heading.
 class AdminProduct(admin.ModelAdmin):
@@ -9,7 +10,11 @@ class AdminProduct(admin.ModelAdmin):
 
 class AdminCategory(admin.ModelAdmin):
     list_display = ['name']
+
+class AdminCustomer(admin.ModelAdmin):
+    list_display = ['email']
     
 # Register your models here.
 admin.site.register(Product, AdminProduct)
 admin.site.register(Category, AdminCategory)
+admin.site.register(Customer, AdminCustomer)
--- stores/views.py
+++ stores/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 
 from .models.products import Product
 from .models.category import Category
+from .models.customer import Customer
 # Create your views here.
 
 def index(request):
@@ -22,4 +23,21 @@ def index(request):
     return render(request, 'index.html', data)
 
 def signup(request):
-    return render(request, 'signup.html')
+    if request.method == 'GET':
+        return render(request, 'signup.html')
+    else:
+        postData = request.POST
+        first_name = postData.get('fname')
+        last_name = postData.get('lname')
+        email = postData.get('email')
+        password= postData.get('password')
+        phone = postData.get('phone')
+        print(first_name, last_name, email, password, phone)
+        customer = Customer(first_name = first_name,
+                            last_name = last_name,
+                            email = email,
+                            password = password,
+                            phone = phone)
+        customer.register()
+        # return HttpResponse(request.POST.get('email'))
+        return HttpResponse("signup success")
