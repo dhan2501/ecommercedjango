c6a0b0ce5e3a7070ab08bad9ea712cc921a84990
Dhananjay Gupta <82753264+dhan2501@users.noreply.github.com>
2024-03-21 21:28:12 +0530

    Navbar modification UI

--- Ecomshop/settings.py
+++ Ecomshop/settings.py
@@ -139,3 +139,9 @@ MEDIA_ROOT = BASE_DIR
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'static'
+#     ]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
