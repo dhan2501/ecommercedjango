33d990729890cdec388ad53dc5439c6c5b27a176
Dhananjay Gupta <82753264+dhan2501@users.noreply.github.com>
2024-03-22 12:12:34 +0530

    work on add to cart module

--- stores/views/home.py
+++ stores/views/home.py
@@ -8,12 +8,28 @@ class Index(View):
     
     def post(self, request):
         product = request.POST.get('product')
-        print(product)
+        # print(product)
+        cart = request.session.get('cart')
+        if cart:
+            # cart[product] = 1
+            quantity = cart.get(product)
+            if quantity:
+                cart[product] = quantity + 1
+            else:
+                cart[product] = 1
+
+        else:
+            cart={}
+            cart[product] = 1
+
+        request.session['cart'] = cart
+        print('cart: ',request.session['cart'])
         return redirect('homepage')
 
 
     def get(self, request):
-        products =None
+        products = None
+        # request.session.clear()
         categories = Category.get_all_categories()
         # print(request.GET['category'])
         categoryID = request.GET.get('category')
