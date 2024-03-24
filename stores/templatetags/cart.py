3e2b0ea6f3a7fe1aa1120d52f18338026d5a7949
Dhananjay Gupta <82753264+dhan2501@users.noreply.github.com>
2024-03-24 10:54:11 +0530

    add cart button and quantity with workong on filter qunatity

--- stores/templatetags/cart.py
+++ stores/templatetags/cart.py
@@ -12,3 +12,14 @@ def is_in_cart(product, cart):
     # print(keys)
     # print(product, cart)
     return False
+
+@register.filter(name='cart_quantity')
+
+def is_in_cart(product, cart):
+    keys = cart.keys()
+    for id in keys:
+        if int(id) == product.id:
+            return cart.get(id)
+    # print(keys)
+    # print(product, cart)
+    return 0
